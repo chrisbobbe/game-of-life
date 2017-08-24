f426029e2b54c723691a8ba7754701b24cbaa123
Chris Bobbe <csbobbe@gmail.com>
2017-08-24 19:39:51 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var c = document.getElementById("game");
+var ctx = c.getContext("2d");
+const width = ctx.width;
+const height = ctx.height;
+const cellWidth = 10;
+const cellHeight = 10;
+
+var grid = function(width, height, cellWidth, cellHeight) {
+  return []
+}
