765a03d34f2085e91cdbb5bf650b7c68635f038b
Michael Noronha <michaeltnoronha@gmail.com>
2017-08-24 22:29:30 -0400

    WIP update/render system

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
 var c = document.getElementById("game");
 var ctx = c.getContext("2d");
+
 const WIDTH = c.width;
 const HEIGHT = c.height;
 const CELL_WIDTH = 10;
 const CELL_HEIGHT = 10;
 
+const DEAD = 0;
+const ALIVE = 1;
+
 var mouseDown = false;
 var mousePos = {x:0, y:0};
 
+
 function SimState() {
-    this.state = Array(Math.floor(WIDTH/CELL_WIDTH)).fill(0);
+    this.renderStack = [];
+    this.state = Array(Math.floor(WIDTH/CELL_WIDTH));
     for (let w = 0; w < Math.floor(WIDTH/CELL_WIDTH); w++) {
-      this.state[w] = Array(Math.floor(HEIGHT/CELL_HEIGHT)).fill(0);
+      this.state[w] = Array(Math.floor(HEIGHT/CELL_HEIGHT)).fill(DEAD);
     }
 
     this.updateState = function() {
       if (mouseDown) {
-        ctx.fillStyle="#FF0000";
-
         var wInd = Math.floor(mousePos.x/CELL_WIDTH);
         var hInd = Math.floor(mousePos.y/CELL_HEIGHT);
         var xPos = Math.floor(mousePos.x/CELL_WIDTH)*CELL_WIDTH;
         var yPos = Math.floor(mousePos.y/CELL_HEIGHT)*CELL_HEIGHT;
 
-        this.state[wInd][hInd] = ctx.fillRect(xPos,yPos,CELL_WIDTH, CELL_HEIGHT);
+        this.state[wInd][hInd] = ALIVE;
+        this.renderStack.push([wInd,hInd]);
       }
       ctx.stroke();
+
     };
 
-    this.render = function() { this.state.forEach(function(arr,w){
-        arr.forEach(function(cell,h){
-          ctx.rect(w*CELL_WIDTH,h*CELL_HEIGHT,CELL_WIDTH,CELL_HEIGHT);
-        });
-      });
+    this.render = function() {
+      while(this.renderStack.length) {
+        let wInd, hInd = this.renderStack.pop();
+        if (this.state[wInd][hInd]) {
+          ctx.fillStyle = "#0f0";
+        } else {
+          ctx.fillStyle = "#000";
+        }
+        ctx.fillRect(w*CELL_WIDTH,h*CELL_HEIGHT,CELL_WIDTH,CELL_HEIGHT);
+      }
       ctx.stroke();
     };
 }
@@ -48,8 +59,6 @@ function getMousePos(canvas, event) {
 
 state = new SimState();
 state.render();
-console.log(state.state);
-
 
 c.addEventListener('mousedown', function(event) {
   console.log("mouse presed");
@@ -61,7 +70,6 @@ c.addEventListener('mousemove', function(event) {
     y: getMousePos(c, event).y
   };
   state.updateState();
-  // console.log("hello");
 });
 c.addEventListener('click', function(event) {
   console.log("mouse released");
