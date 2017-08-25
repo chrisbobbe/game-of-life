93b7367174b86b9dff090fc98b9c3826b2d13535
Chris Bobbe <csbobbe@gmail.com>
2017-08-24 20:45:39 -0400

    add event listeners

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const height = c.height;
 const cellWidth = 10;
 const cellHeight = 10;
 
+var mouseDown = false
+var mousePos = {x:0, y:0}
+
 function SimState() {
     this.state = Array(Math.floor(width/cellWidth)).fill(0);
     for (let w = 0; w < Math.floor(width/cellWidth); w++) {
@@ -13,19 +16,47 @@ function SimState() {
 
 
     this.updateState = function() {
-
+      if (mouseDown) {
+        ctx.fillRect(mousePos.x*cellWidth, mousePos.y*cellHeight, cellWidth, cellHeight);
+      };
+      console.log("hi");
     };
 
     this.render = function() {
         this.state.forEach(function(arr,w){
             arr.forEach(function(cell,h){
-                ctx.rect(w*10,h*10,cellWidth,cellHeight);
+                ctx.rect(w*cellWidth,h*cellHeight,cellWidth,cellHeight);
             });
         });
         ctx.stroke();
     };
 }
 
+function getMousePos(canvas, event) {
+  var rect = c.getBoundingClientRect();
+  return {
+    x: event.clientX - rect.left,
+    y: event.clientY - rect.top
+  };
+}
+
+
 state = new SimState();
 state.render();
 console.log(state.state);
+
+
+c.addEventListener('mousedown', function(event) {
+  mouseDown = true;
+});
+c.addEventListener('mousemove', function(event) {
+  mousePos = {
+    x: getMousePos(c, event).x,
+    y: getMousePos(c, event).y
+  };
+  state.updateState();
+  console.log("hello");
+});
+c.addEventListener('click', function(event) {
+  mouseDown = false
+});
