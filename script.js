06db78801d8806216a9b01be772edc078cc227e9
Chris Bobbe <csbobbe@gmail.com>
2017-08-24 20:49:31 -0400

    uppercased constants

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 var c = document.getElementById("game");
 var ctx = c.getContext("2d");
-const width = c.width;
-const height = c.height;
-const cellWidth = 10;
-const cellHeight = 10;
+const WIDTH = c.WIDTH;
+const HEIGHT = c.HEIGHT;
+const CELL_WIDTH = 10;
+const CELL_HEIGHT = 10;
 
 var mouseDown = false
 var mousePos = {x:0, y:0}
 
 function SimState() {
-    this.state = Array(Math.floor(width/cellWidth)).fill(0);
-    for (let w = 0; w < Math.floor(width/cellWidth); w++) {
-        this.state[w] = Array(Math.floor(height/cellHeight)).fill(0);
+    this.state = Array(Math.floor(WIDTH/CELL_WIDTH)).fill(0);
+    for (let w = 0; w < Math.floor(WIDTH/CELL_WIDTH); w++) {
+        this.state[w] = Array(Math.floor(HEIGHT/CELL_HEIGHT)).fill(0);
     }
 
 
     this.updateState = function() {
       if (mouseDown) {
-        ctx.fillRect(mousePos.x*cellWidth, mousePos.y*cellHeight, cellWidth, cellHeight);
+        ctx.fillRect(mousePos.x*CELL_WIDTH, mousePos.y*CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT);
       };
       console.log("hi");
     };
@@ -25,7 +25,7 @@ function SimState() {
     this.render = function() {
         this.state.forEach(function(arr,w){
             arr.forEach(function(cell,h){
-                ctx.rect(w*cellWidth,h*cellHeight,cellWidth,cellHeight);
+                ctx.rect(w*CELL_WIDTH,h*CELL_HEIGHT,CELL_WIDTH,CELL_HEIGHT);
             });
         });
         ctx.stroke();
